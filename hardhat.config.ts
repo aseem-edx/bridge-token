b46c98f7483fa4d47399f9baf6362a7ea4dcfc9b
Aseem PM <a.palumannil@edexa.team>
2024-05-17 11:43:11 +0530

    added hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,20 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
+const PK = vars.get('PK')
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    "arbitrum-sepolia": {
+      url: "https://sepolia-rollup.arbitrum.io/rpc",
+      accounts: [PK]
+    },
+    "bsc-testnet": {
+      url: "https://data-seed-prebsc-2-s1.binance.org:8545/",
+      accounts: [PK]
+    }
+  }
 };
 
 export default config;
